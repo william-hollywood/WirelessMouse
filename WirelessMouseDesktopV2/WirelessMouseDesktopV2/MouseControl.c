1f73da37597d89f2a537d4e21bfae94f4bc45276
William Hollywood <will@oceanplait.com>
2023-03-05 09:25:27 +1300

    Migrated to standard limits

--- WirelessMouseDesktopV2/WirelessMouseDesktopV2/MouseControl.c
+++ WirelessMouseDesktopV2/WirelessMouseDesktopV2/MouseControl.c
@@ -2,11 +2,9 @@
 
 #include <windows.h>
 #include <WinUser.h>
+#include <limits.h>
 
 #include "Encoding.h"
-
-#define SHORT_MAXVALUE 32767
-#define SHORT_MINVALUE -32768
 
 int32_t lastX;
 int32_t lastY;
@@ -14,24 +12,24 @@ uint8_t holdPress = 0;
 double SCALE = 2.5;
 
 void handle_mouse(short dx, short dy) {
-    if (dx == SHORT_MAXVALUE && dy == SHORT_MAXVALUE)  // left click
+    if (dx == SHRT_MAX && dy == SHRT_MAX)  // left click
     {
         if (!holdPress) {
             mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
             mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
         }
-    } else if (dx == SHORT_MINVALUE && dy == SHORT_MINVALUE)  // right click
+    } else if (dx == SHRT_MIN && dy == SHRT_MIN)  // right click
     {
         if (!holdPress) {
             mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
             mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
         }
-    } else if (dx == SHORT_MAXVALUE && dy == SHORT_MINVALUE)  // left down toggle
+    } else if (dx == SHRT_MAX && dy == SHRT_MIN)  // left down toggle
     {
         if (!holdPress)  // check this
             mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
         holdPress = 1;
-    } else if (dx == SHORT_MINVALUE && dy == SHORT_MAXVALUE)  // left up toggle
+    } else if (dx == SHRT_MIN && dy == SHRT_MAX)  // left up toggle
     {
         if (holdPress)  // check this
             mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
